4d9a939069d4b4a95336da8f9668695452388d83
Kevin C McKenna <kevmckenna@gmail.com>
2017-08-29 15:36:50 -0700

    equals button and listener

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var readout = document.querySelector('#readout')
 // singular use for var for single item
 var digits = document.querySelectorAll('.digit')
 // plural use for var for multiple items
+var operators = document.querySelectorAll('.operator')
+
+var equalsBtn = document.querySelector('#equals')
 
 //for each digit, do a function...
 digits.forEach(function(btn){
@@ -9,11 +12,10 @@ digits.forEach(function(btn){
     btn.addEventListener('click', function() {
         // 'this' is the button being clicked...
         console.log(this.innerText)
+        // below changes the text to update in readout div to show 'this' button
         readout.innerText += this.innerText
     })
 })
-
-var operators = document.querySelectorAll('.operator')
 
 operators.forEach(function(btn){
     btn.addEventListener('click', function() {
@@ -21,3 +23,19 @@ operators.forEach(function(btn){
         readout.innerText += this.innerText
     })
 })
+
+// equalsBtn.addEventListener('click', function() {
+//     console.log(readout.innerText)
+//     readout.innerText = eval(readout.innerText)
+// })
+
+var equalsBtn = [digits, operators];
+    equalsBtn.forEach(function(btn)) {
+
+    btn.addEventListener('click', function() {
+    for(var i = 0; i < equalsBtn.length; i++){
+    console.log(this[equalsBtn[i]].innerText);
+    readout.innerText += this.innerText
+    }
+})
+}
