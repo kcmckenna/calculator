f3e1b1fb9624e92a0528db6c55796c93a0ba34a3
Kevin C McKenna <kevmckenna@gmail.com>
2017-08-29 15:08:45 -0700

    operator buttons

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 var readout = document.querySelector('#readout')
+// singular use for var for single item
 var digits = document.querySelectorAll('.digit')
+// plural use for var for multiple items
 
 //for each digit, do a function...
-digits.forEach(function(buttn, index){
+digits.forEach(function(btn){
     //add an event listener to each button...
-    buttn.addEventListener('click', function() {
+    btn.addEventListener('click', function() {
         // 'this' is the button being clicked...
+        console.log(this.innerText)
+        readout.innerText += this.innerText
+    })
+})
+
+var operators = document.querySelectorAll('.operator')
+
+operators.forEach(function(btn){
+    btn.addEventListener('click', function() {
         console.log(this.innerText)
         readout.innerText += this.innerText
     })
